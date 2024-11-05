bc3ca8a8333a28fd6eea3d6ccf1ea38c7ae41c28
Marcelo Gutiérrez <121756817+Marusero110@users.noreply.github.com>
2024-11-05 15:00:12 -0400

    Add files via upload

---
+++ Arrays/Array 1/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] fraudulentOrderIDs = ["A123", "B456", "C789"];
+
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+// Reassign the first value in fraudulentOrderIDs
+fraudulentOrderIDs[0] = "F000";
+
+Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
