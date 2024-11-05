f156e1c7d8124ab3ad9e86663e4b7d2c09890b68
Marcelo Gutiérrez <121756817+Marusero110@users.noreply.github.com>
2024-11-05 15:18:07 -0400

    Add files via upload

---
+++ Removing Code Blocks/Program.cs
@@ -0,0 +1,13 @@
+﻿// Code sample 1
+bool flag = true;
+if (flag) Console.WriteLine(flag);
+
+// Code sample 2
+string name = "Steve";
+if (name == "Bob") {
+    Console.WriteLine("Found Bob");
+} else if (name == "Steve") {
+    Console.WriteLine("Found Steve");
+} else {
+    Console.WriteLine("Found Chuck");
+}
