3de54f47d6f3809d7cca77f0c97277a047ef1425
Marcelo Gutiérrez <121756817+Marusero110@users.noreply.github.com>
2024-11-05 15:40:01 -0400

    Add files via upload

---
+++ RPG Battle Challenge/Program.cs
@@ -0,0 +1,18 @@
+﻿int hero = 10;
+int monster = 10;
+
+Random dice = new();
+
+do {
+    int roll = dice.Next(1, 11);
+    monster -= roll;
+    Console.WriteLine($"Monster was damaged and lost {roll} health points and now has {monster} health points.");
+
+    if (monster <= 0) continue;
+
+    roll = dice.Next(1, 11);
+    hero -= roll;
+    Console.WriteLine($"Hero was damaged and lost {roll} health points and now has {hero} health points.");
+} while ((hero > 0) && (monster > 0));
+
+Console.WriteLine(hero > monster ? "Hero wins!" : "Monster wins!");
