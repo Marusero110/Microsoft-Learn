19fa9e0f20eb4f6a5a432873dca287db04f42b08
Marcelo Gutiérrez <121756817+Marusero110@users.noreply.github.com>
2024-11-05 15:50:20 -0400

    Add files via upload

---
+++ Reference Types/Program.cs
@@ -0,0 +1,22 @@
+﻿// Value Types
+
+int val_A = 2;
+int val_B = val_A;
+val_B = 5;
+
+Console.WriteLine("--Value Types--");
+Console.WriteLine($"val_A: {val_A}");
+Console.WriteLine($"val_B: {val_B}");
+
+Console.WriteLine();
+
+// Reference Types
+
+int[] ref_A = new int[1];
+ref_A[0] = 2;
+int[] ref_B = new int[1];
+ref_B[0] = 5;
+
+Console.WriteLine("--Reference Types--");
+Console.WriteLine($"ref_A[0]: {ref_A[0]}");
+Console.WriteLine($"ref_B[0]: {ref_B[0]}");
